8a80f90adfdba253fab4b72f67a11430691a7276
PAJOT-Basile <156067026+PAJOT-Basile@users.noreply.github.com>
2024-05-16 13:25:35 +0200

    Update Figure_3.r

--- Report/Figure_3.r
+++ Report/Figure_3.r
@@ -7,7 +7,7 @@ anyLib(c("tidyverse", "adegenet", "vcfR", "readxl", "statgenGWAS", "ggforce", "g
 ################## Useful functions  ##################
 "%!in%" <- function(x, y){!(x %in% y)}
 
-source("/shared/projects/pacobar/finalresult/bpajot/genomic_analysis/scripts/01_Filtering_stats_vcf/Functions_optimise_plot_clines.r")
+source("../General_scripts/Functions_optimise_plot_clines.r")
 
 ################################ Useful variables ################################
 # Color palette to be reused everywhere with the shell size
