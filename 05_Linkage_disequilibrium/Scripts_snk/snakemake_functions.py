0e9f7a46b47441c5c64112580d4f5fec71010274
Basile Pajot <156067026+PAJOT-Basile@users.noreply.github.com>
2025-03-06 18:37:32 +0100

    Create snakemake_functions.py

---
+++ 05_Linkage_disequilibrium/Scripts_snk/snakemake_functions.py
@@ -0,0 +1,19 @@
+# Description: This script contains functions that are loaded when the snakemake is initiated
+# Modules required: numpy
+# Date: 23 September 2024
+# Author: Basile Pajot
+#########################################################################################################################
+#
+# Libraries
+import gzip
+
+########################  Functions   ###############################
+######################## Get chromosome names  ###############################
+
+
+def get_chromosome_name(path_chromosome_file):
+    list_chromosomes = []
+    with open(path_chromosome_file, "r") as f:
+        for line in f.readlines():
+            list_chromosomes.append(line.strip("\n"))
+    return (list_chromosomes)
