cb4cd1de229bf51369830d36ebd5ce8136acd20b
Basile Pajot <156067026+PAJOT-Basile@users.noreply.github.com>
2025-03-11 17:26:36 +0100

    Update Genome_wide_Fst.r
    Add structure and remove unused code

--- 07_Genome_wide_FST/Genome_wide_Fst.r
+++ 07_Genome_wide_FST/Genome_wide_Fst.r
@@ -4,42 +4,18 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(characters = libraries, character.only = TRUE)
 rm(libraries)
 
-
-################## Useful function  ##################
-
-################## Import vcf  ##################
-# data <- read.vcfR("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/02_Filter_VCF/09_Maf_thin/VCF_File.vcf.gz") %>% 
-#   vcfR2genind()
-
-# data@pop <- (data@tab %>% rownames %>% str_split_fixed(., "_", 7))[, 3] %>% str_remove_all("n") %>% as.factor
-
-# ################## Compute genome wide for all the population  ##################
-# # First, import the VCF file as a genlight object
-# data2 <- read.vcfR("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/02_Filter_VCF/09_Maf_thin/VCF_File.vcf.gz") %>% 
-#   vcfR2genlight()
-
-# data2@pop <- (data2@ind.names %>% str_split_fixed(., "_", 7))[, 3] %>% str_remove_all("n") %>% as.factor
-# Fst <- stamppFst(data2, nboots = 10000, percent = 95, nclusters = 3)
-
-# Fst$Fsts %>% 
-#   write.table("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Population_wide_Fst.tsv",
-#               sep = "\t", col.names = TRUE, row.names = TRUE, quote = FALSE)
-# Fst$Pvalues %>% 
-#   write.table("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Population_wide_Pvalues.tsv",
-#               sep = "\t", col.names = TRUE, row.names = TRUE, quote = FALSE)
-
-################## Re-sample a vcf file with the reference individuals  ##################
-# system2("/shared/software/miniconda/envs/vcftools-0.1.16/bin/vcftools",
-#         args = paste0("--gzvcf /shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/02_Filter_VCF/09_Maf_thin/VCF_File.vcf.gz",
-#                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/France_exposed.txt",
-#                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/France_sheltered.txt",
-#                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/Sweden_exposed.txt",
-#                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/Sweden_sheltered.txt",
-#                       " --recode",
-#                       " --stdout | gzip -c > /shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Reference_indivs.vcf.gz"))
+################## Sample a vcf file with the reference individuals  ##################
+ system2("/shared/software/miniconda/envs/vcftools-0.1.16/bin/vcftools",
+         args = paste0("--gzvcf /shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/02_Filter_VCF/09_Maf_thin/VCF_File.vcf.gz",
+                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/France_exposed.txt",
+                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/France_sheltered.txt",
+                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/Sweden_exposed.txt",
+                       " --keep /shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/Sweden_sheltered.txt",
+                       " --recode",
+                       " --stdout | gzip -c > /shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Reference_indivs.vcf.gz"))
 
 # First, import the VCF file as a genlight object
-data3 <- read.vcfR("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Reference_indivs.vcf.gz") %>% 
+data <- read.vcfR("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Reference_indivs.vcf.gz") %>% 
   vcfR2genlight()
 
 ref_indivs <- read.table("/shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/Data/Reference_indivs_expos/France_exposed.txt", header = FALSE,
@@ -55,15 +31,15 @@ ref_indivs <- read.table("/shared/projects/pacobar/finalresult/bpajot/Stage_Rosc
                    sep = "\t", col.names = "Sample_Name") %>% 
           mutate(Exposition = "Sw_Shl"))
 
-data3@pop <- data3@ind.names %>%
+data@pop <- data@ind.names %>%
   as.data.frame %>% 
   rename(Sample_Name = ".") %>% 
   left_join(ref_indivs, by = "Sample_Name") %>% 
   pull(Exposition) %>% 
   as.factor
 
-
-Fst_ecotypes <- stamppFst(data3, nboots = 10000, percent = 95, nclusters = 3)
+############### Compute Fst between reference individuals ###############
+Fst_ecotypes <- stamppFst(data, nboots = 10000, percent = 95, nclusters = 3)
 
 Fst_ecotypes$Fsts %>% 
   write.table("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/07_Genome_wide_FST/Ecotype_Fst.tsv",
