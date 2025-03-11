8ca5a9e16fdae9eeabc9c03d0f44a3a2f2429162
Basile Pajot <156067026+PAJOT-Basile@users.noreply.github.com>
2025-03-11 17:20:38 +0100

    Update Figure_LD_heatmap.r
    Removing unnecessary code, struture the code and add comments to explain what is done in the different steps of the script

--- 05_Linkage_disequilibrium/Figure_LD_heatmap.r
+++ 05_Linkage_disequilibrium/Figure_LD_heatmap.r
@@ -1,45 +1,60 @@
 
-#libraries <- c("tidyverse")
-#pacman::p_load(char = libraries, character.only = TRUE)
-#rm(libraries)
-
+############### Import libraries ###############
 library(tidyverse)
+
+############### Useful functions ###############
 source("/shared/projects/pacobar/finalresult/bpajot/Stage_Roscoff/scripts/A_Genetic_analysis/General_scripts/Functions_optimise_plot_clines.r")
+
+############### Useful variables ###############
 # Make a function to sample colours in a user-defined scale
 colfunc <- colorRampPalette(colors = c("#813d32","#ef852f","#fdc441" ,"#e3cfb4") %>% rev, bias = 2)
 
-# Import and bind the files that are in the same directory
+############### Plotting the LD per population ###############
+# Loop over the populations
 for (population in c("Sweden", "France")){
   print(population)
-  
+
+  # Get the name of the directory where the LD values are stored 
   Storage_directory <- paste0("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/05_LD_computations/", population, "/LD_values/")
-  
-  complete_df <- data.frame()
+
+  # Initialise the output data frame containig the LD and iterate over the files in the storage directory to load them successively
+  LD_df <- data.frame()
   for (file in list.files(Storage_directory)){
     print(paste0("    ", file))
+
+    # Load the table of the considered chromosome
     temp_df <- read.table(paste0(Storage_directory, file), sep = "\t", header = TRUE) %>% 
       rename(Chromosome = CHR,
              LD = R.2) %>% 
       mutate(Position1 = (POS1 / 1e5) %>% round,
              Position2 = (POS2 / 1e5) %>% round) %>% 
+    # Calculate the mean LD per bin (1e5 bp) to make the plotting easier
       group_by(Position1, Position2) %>% 
       summarize(mean_LD = mean(LD), .groups = "drop_last") %>% 
       ungroup
+
+    # Get the name of the chromosome
     chromosome <- (file %>% 
                      str_split_fixed("\\.", 2))[, 1]
-    
-    complete_df <- add_table_to_df_in_iteration(complete_df, temp_df %>% 
+
+    # Add the loaded LD info for the chromosome to the complete dataframe
+    LD_df <- add_table_to_df_in_iteration(LD_df, temp_df %>% 
                                                   mutate(Chromosome = chromosome))
   }
+  
+  # Clear the environment a bit
   rm(temp_df, chromosome, file)
+  
   # Plot the output and save it
   print("    Plotting")
-  (complete_df %>% 
+  (LD_df %>% 
+   # Reorder the chromosome order for the facets to be in the correct order
       mutate(Chromosome = Chromosome %>% 
-               factor(levels = complete_df %>% select(Chromosome) %>% 
+               factor(levels = LD_df %>% select(Chromosome) %>% 
                         unique %>%
                         arrange(as.numeric(gsub("\\D*(\\d+).*", "\\1", Chromosome))) %>% 
                         as.vector %>% unname %>% unlist)) %>% 
+   # Plot the output
       ggplot() +
       geom_tile(aes(x = Position1, y = Position2, fill = mean_LD)) +
       scale_fill_gradientn(name = "Mean LD\n(R²)",
@@ -50,6 +65,7 @@ for (population in c("Sweden", "France")){
             legend.key.size = unit(1.5, "cm")) +
       labs(x = "Position along the chromosome (*1e5)",
            y = "Position along the chromosome (*1e5)")) %>% 
+  # Save the graph into a file
     ggsave(plot = ., filename = paste0("/shared/projects/pacobar/finalresult/Littorina_WGS_illumina/Sweden_France_parallelism/05_LD_computations/", population, "/LD_heatmaps/LD_heatmaps.png"),
            device = "png", width = 2000, height = 1800, units = "px", scale = 5, limitsize = FALSE)
 }
